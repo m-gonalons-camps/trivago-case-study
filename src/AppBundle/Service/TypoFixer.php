6a1613ce02344cbbfef0032de8f5272f8a226d89
Marcos Goñalons <m.gonalons.camps@gmail.com>
2017-07-30 19:15:54 -0400

    Finished TypoFixer with tests.

--- src/AppBundle/Service/TypoFixer.php
+++ src/AppBundle/Service/TypoFixer.php
@@ -2,12 +2,46 @@
 
 namespace AppBundle\Service;
 
-use Chencha\Pspell\Pspell;
-
 class TypoFixer implements ITypoFixer {
 
     public function fix(string &$stringToFix) : void {
-        
+        $words = str_word_count($stringToFix, 1);
+
+        foreach ($words as $word) {
+            $suggestions = $this->getSuggestions($word);
+            if (! count($suggestions)) continue;
+
+            $bestSuggestion = $this->getBestSuggestion($word, $suggestions);
+            $stringToFix = str_replace($word, $bestSuggestion, $stringToFix);
+        }
+    }
+
+    private function getSuggestions(string $word) : array {
+        $dictionary = pspell_new("en");
+
+        // This returns TRUE if the word does not have any typos.
+        if (pspell_check($dictionary, $word)) return [];
+
+        return pspell_suggest($dictionary, $word);
+    }
+
+    private function getBestSuggestion(string $word, array $suggestions) : string {
+        $bestSuggestion = [
+            'similarityPercentage' => 0,
+            'suggestion' => $word
+        ];
+        foreach ($suggestions as $suggestion) {
+            $suggestion = strtolower($suggestion);
+            $similarityPercentage = 0;
+            similar_text($word, $suggestion, $similarityPercentage);
+
+            if ($similarityPercentage > $bestSuggestion['similarityPercentage']) {
+                $bestSuggestion['similarityPercentage'] = $similarityPercentage;
+                $bestSuggestion['suggestion'] = $suggestion;
+            }
+        }
+
+        return $bestSuggestion['suggestion'];
     }
 
 }
